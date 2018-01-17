14134692099486f40aa90485eff15230460e1dd1
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-17 08:27:18 -0500

    Create face.py

---
+++ face.py
@@ -0,0 +1,10 @@
+#Matt Westelman
+#1/17/18
+#face.py - making a face
+
+print("   xxxxx ")
+print("  x      x")
+print("((  o  o  ))")
+print("  |   V   |")
+print("  |  ===  |")
+print("  ---------")
