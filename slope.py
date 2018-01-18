8d0af37a60de8b05076968dadaf7ca7c682dd5c5
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-18 08:51:30 -0500

    Create slope.py

---
+++ slope.py
@@ -0,0 +1,12 @@
+#Matt Westelman
+#1/18/18
+#slope.py - What is the slope?
+
+xPoint1 = float(input("x1 = "))
+yPoint1 = float(input("y1 = "))
+xPoint2 =float(input("x2 = "))
+yPoint2 = float(input("y2 = "))
+
+print("slope = ", round((xPoint1-xPoint2)/(yPoint1 - yPoint2), 3))
+slope = round((xPoint1-xPoint2)/(yPoint1 - yPoint2), 3)
+print("b = 
