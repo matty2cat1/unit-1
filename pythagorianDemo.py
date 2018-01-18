fa099f9e15b3c8da0d6a8230950a5d81c2165d76
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-01-18 08:10:19 -0500

    Create pythagorianDemo.py

---
+++ pythagorianDemo.py
@@ -0,0 +1,9 @@
+#Matt Westelman
+#1/18/18
+#pythagorianDemo.py - How to import objects
+
+from math import sqrt
+
+a = float(input("side a "))
+b = float(input("side b "))
+print("the hypotanuse is", sqrt(a**2 + b**2))
